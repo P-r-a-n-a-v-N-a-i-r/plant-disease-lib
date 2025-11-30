b8ff8cc0390e8e2f85b3eed06077f34e71bc80fc
Pranav <nairpranavsivadasan@gmail.com>
2025-11-30 16:37:33 +0000

    version updated

--- setup.py
+++ setup.py
@@ -1,28 +1,41 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='plant_disease_lib',
-    version='0.1.3',
-    author='Pranav Nair',
-    author_email='pranavshivannair@gmail.com',
-    description='A library for plant disease detection using deep learning',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/P-r-a-n-a-v-N-a-i-r/plant-disease-lib',
+    name="plant_disease_lib",
+    version="0.1.4",
+    author="Pranav Nair",
+    author_email="pranavshivannair@gmail.com",
+    description="A library for plant disease detection using deep learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/P-r-a-n-a-v-N-a-i-r/plant-disease-lib",
     packages=find_packages(),
+    python_requires=">=3.8",
     install_requires=[
-        'pillow',
-        'numpy',
-        'pandas',
-        'torch',
-        'torchvision'
+        "numpy>=1.26.0",
+        "pandas==1.2.4",
+        "Pillow==8.2.0",
+        "python-dateutil==2.8.1",
+        "pytz==2021.1",
+        "six==1.15.0",
+        "typing-extensions==3.7.4.3",
+        "torch==1.8.1+cpu",
+        "torchvision==0.9.1+cpu"
+
     ],
-    python_requires='>=3.7',
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     include_package_data=True,
+    package_data={
+        "plant_disease_lib": ["*.csv"],
+    },
 )
